b41f77d7e0e56e8dc6e9009761edeff66cabb9b6
xfgczzg <xfgczzg@163.com>
2018-04-05 08:40:24 +0800

    清除掉不需要的项目

---
+++ SDKLibrary/src/main/java/com/zhiyangstudio/sdklibrary/Const.java
@@ -0,0 +1,9 @@
+package com.zhiyangstudio.sdklibrary;
+
+/**
+ * Created by zzg on 2018/4/5.
+ */
+
+public class Const {
+    public static final String LOG_TAG = "test";
+}
---
+++ SDKLibrary/src/main/java/com/zhiyangstudio/sdklibrary/NetUtils.java
@@ -0,0 +1,12 @@
+package com.zhiyangstudio.sdklibrary;
+
+/**
+ * Created by zzg on 2018/4/5.
+ */
+
+public class NetUtils {
+    public static boolean hasNetAvaliable() {
+        LogUtils.log("network not avaliable");
+        return false;
+    }
+}
