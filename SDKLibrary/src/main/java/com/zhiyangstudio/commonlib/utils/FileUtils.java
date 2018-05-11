afd8db9addc5f33c57cce2e9381d6657168e1be9
xfgczzg <xfgczzg@163.com>
2018-05-11 19:48:53 +0800

    代码整理

--- SDKLibrary/src/main/java/com/zhiyangstudio/commonlib/utils/FileUtils.java
+++ SDKLibrary/src/main/java/com/zhiyangstudio/commonlib/utils/FileUtils.java
@@ -1,7 +1,18 @@
 package com.zhiyangstudio.commonlib.utils;
 
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import com.blankj.utilcode.util.LogUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.FileChannel;
+import java.text.DecimalFormat;
 
 /**
  * Created by zzg on 2018/4/25.
@@ -20,5 +31,171 @@ public class FileUtils {
             IoUtils.close(inputStream);
         }
         return content;
+    }
+
+    /*
+     * Java文件操作 获取文件扩展名
+     */
+    public static String getExtensionName(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot > -1) && (dot < (filename.length() - 1))) {
+                return filename.substring(dot + 1);
+            }
+        }
+        return filename;
+    }
+
+    /*
+     * Java文件操作 获取不带扩展名的文件名
+     */
+    public static String getFileNameNoEx(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot > -1) && (dot < (filename.length()))) {
+                return filename.substring(0, dot);
+            }
+        }
+        return filename;
+    }
+
+
+    /**
+     * 创建根缓存目录
+     *
+     * @return
+     */
+    public static String createRootPath(Context context) {
+        String cacheRootPath = "";
+        if (isSdCardAvailable()) {
+            // /sdcard/Android/data/<application package>/cache
+            cacheRootPath = context.getExternalCacheDir().getPath();
+        } else {
+            // /data/data/<application package>/cache
+            cacheRootPath = context.getCacheDir().getPath();
+        }
+        return cacheRootPath;
+    }
+
+    public static boolean isSdCardAvailable() {
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+    }
+
+
+    /**
+     * 递归创建文件夹
+     *
+     * @param file
+     * @return 创建失败返回""
+     */
+    public static String createFile(File file) {
+        try {
+            if (file.getParentFile().exists()) {
+                LogUtils.i("----- 创建文件" + file.getAbsolutePath());
+                file.createNewFile();
+                return file.getAbsolutePath();
+            } else {
+                createDir(file.getParentFile().getAbsolutePath());
+                file.createNewFile();
+                LogUtils.i("----- 创建文件" + file.getAbsolutePath());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 递归创建文件夹
+     *
+     * @param dirPath
+     * @return 创建失败返回""
+     */
+    public static String createDir(String dirPath) {
+        try {
+            File file = new File(dirPath);
+            if (file.getParentFile().exists()) {
+                LogUtils.i("----- 创建文件夹" + file.getAbsolutePath());
+                file.mkdir();
+                return file.getAbsolutePath();
+            } else {
+                createDir(file.getParentFile().getAbsolutePath());
+                LogUtils.i("----- 创建文件夹" + file.getAbsolutePath());
+                file.mkdir();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dirPath;
+    }
+
+
+    /**
+     * 转换文件大小
+     *
+     * @param fileLen 单位B
+     * @return
+     */
+    public static String formatFileSizeToString(long fileLen) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        String fileSizeString = "";
+        if (fileLen < 1024) {
+            fileSizeString = df.format((double) fileLen) + "B";
+        } else if (fileLen < 1048576) {
+            fileSizeString = df.format((double) fileLen / 1024) + "K";
+        } else if (fileLen < 1073741824) {
+            fileSizeString = df.format((double) fileLen / 1048576) + "M";
+        } else {
+            fileSizeString = df.format((double) fileLen / 1073741824) + "G";
+        }
+        return fileSizeString;
+    }
+
+    /**
+     * 文件拷贝
+     *
+     * @param src  源文件
+     * @param desc 目的文件
+     */
+    public static void fileChannelCopy(File src, File desc) {
+        //createFile(src);
+        createFile(desc);
+        FileInputStream fi = null;
+        FileOutputStream fo = null;
+        try {
+            fi = new FileInputStream(src);
+            fo = new FileOutputStream(desc);
+            FileChannel in = fi.getChannel();//得到对应的文件通道
+            FileChannel out = fo.getChannel();//得到对应的文件通道
+            in.transferTo(0, in.size(), out);//连接两个通道，并且从in通道读取，然后写入out通道
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fo != null) fo.close();
+                if (fi != null) fi.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 获取指定文件大小
+     *
+     * @return
+     * @throws Exception
+     */
+    public static long getFileSize(File file) throws Exception {
+        long size = 0;
+        if (file.exists()) {
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+            size = fis.available();
+        } else {
+            file.createNewFile();
+            Log.e("获取文件大小", "文件不存在!");
+        }
+        return size;
     }
 }
