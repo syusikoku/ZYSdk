283d84277710c929ef30117e389ddb2914d70c5e
xfgczzg <xfgczzg@163.com>
2018-07-10 19:08:55 +0800

    glide svg图片支持

--- SDKLibrary/src/main/java/com/zysdk/vulture/clib/utils/DisplayUtils.java
+++ SDKLibrary/src/main/java/com/zysdk/vulture/clib/utils/DisplayUtils.java
@@ -7,9 +7,11 @@ import android.graphics.Point;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Display;
+import android.view.Window;
 import android.view.WindowManager;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by zhiyang on 2018/4/8.
@@ -150,5 +152,61 @@ public class DisplayUtils {
 
     private static WindowManager getWindowManager() {
         return (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+    }
+
+
+    //获取屏幕原始尺寸高度，包括虚拟功能键高度
+    public static int getDpi(Context context) {
+        int dpi = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context
+                .WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        @SuppressWarnings("rawtypes")
+        Class c;
+        try {
+            c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, displayMetrics);
+            dpi = displayMetrics.heightPixels;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dpi;
+    }
+
+    /**
+     * 获取 虚拟按键的高度
+     */
+    public static int getBottomStatusHeight(Context context) {
+        int totalHeight = getDpi(context);
+        int contentHeight = getScreenHeight();
+        return totalHeight - contentHeight;
+    }
+
+    /**
+     * 标题栏高度
+     */
+    public static int getTitleHeight(Activity activity) {
+        return activity.getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+    }
+
+    /**
+     * 获得状态栏的高度
+     */
+    public static int getStatusHeight(Context context) {
+
+        int statusHeight = -1;
+        try {
+            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height")
+                    .get(object).toString());
+            statusHeight = context.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusHeight;
     }
 }
